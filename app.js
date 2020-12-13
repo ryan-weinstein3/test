084bbb5f821f570933b8a265f9d376818fb74b09
ryan-weinstein3 <71529800+ryan-weinstein3@users.noreply.github.com>
2020-12-13 18:17:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{ 
+ 
+var data = {name:'Akashdeep', 
+    hobbies:['playing football', 'playing chess', 'cycling']} 
+  
+res.render('home', {data:data}); 
+}); 
+  
+var server = app.listen(4000, function() { 
+    console.log('listining to port 4000') 
+}); 
